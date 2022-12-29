41366bf390f1964b40a96de07679a2ba5e868302
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-29 11:31:57 +0200

    task1
    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart =document.querySelector('[data-start]');
+const btnStop =document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+btnStart.addEventListener('click', onStartClik);
+btnStop.addEventListener('click', onStopClick);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+function onStartClik() {
+    timerId = setInterval(()=>{
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;  
+    }, 1000);
+    btnStart.disabled = true;
+
+};
+
+function onStopClick() {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+}
